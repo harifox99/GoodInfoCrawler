0fbbb3af93ea0282d55e153f02448afce95311ed
harifox99 <aluba0504@gmail.com>
2026-08-30 10:58:53 +0800

    改HTTP Post

--- src/main/java/org/bear/crawler/GoodInfoCrawlerPercent.java
+++ src/main/java/org/bear/crawler/GoodInfoCrawlerPercent.java
@@ -68,25 +68,57 @@ public class GoodInfoCrawlerPercent extends ImportStockID
 			// Step 3.
 			// 開啟 GoodInfo
 			//
-			for (int j = 0; j < wrapperList.size(); j++)
+			for (int j = 0; j < stockList.size(); j++)
 			{
-				STOCK_ID = wrapperList.get(j).getStockID();
+				STOCK_ID = stockList.get(j);				
 				driver.get("https://goodinfo.tw/tw/EquityDistributionClassHis.asp?STOCK_ID=" + STOCK_ID);
 				Thread.sleep(3000);
     			//
     			// Step 4.
     			// 取得資料
     			//
+    			/*
     			System.out.println("Title = " + driver.getTitle());
     			System.out.println("URL = " + driver.getCurrentUrl());
     			String html = driver.getPageSource();
-    			System.out.println("HTML Length = " + html.length());
+    			System.out.println("HTML Length = " + html.length());*/
+				String postString =
+				        "STOCK_ID=" + STOCK_ID
+				        + "&CHT_CAT=WEEK"
+				        + "&STEP=DATA"
+				        + "&DISPLAY_CAT=%E6%8C%81%E6%9C%89%E6%AF%94%E4%BE%8B%E5%8D%80%E9%96%93%E5%88%86%E7%B4%9A%E4%B8%80%E8%A6%BD%28%E5%AE%8C%E6%95%B4%29";
+
+				((JavascriptExecutor) driver).executeScript("window.goodinfoResult = null;");
+
+				String script =
+				        "fetch('https://goodinfo.tw/tw/data/EquityDistributionClassHis.asp', {"
+				      + "method:'POST',"
+				      + "headers:{"
+				      + "'Content-Type':'application/x-www-form-urlencoded'"
+				      + "},"
+				      + "body:'" + postString + "'"
+				      + "})"
+				      + ".then(r => r.text())"
+				      + ".then(d => window.goodinfoResult = d);";
+
+				((JavascriptExecutor) driver).executeScript(script);
+				
+				String html = null;
+				for (int retry = 0; retry < 10; retry++)
+				{
+					Thread.sleep(1000);
+					html = (String) ((JavascriptExecutor) driver).executeScript("return window.goodinfoResult;");
+				    if (html != null)
+				    {
+				        break;
+				    }
+				}
     			//
     			// 5. Parser
     			//
     			GoodInfoDistributionPercentParser parser = new GoodInfoDistributionPercentParser();
     			parser.parse(html, dateString, week, dao, STOCK_ID, true);
-    			Thread.sleep(10000);
+    			Thread.sleep(5000);
 			}
 		}
 		catch (Exception ex)
